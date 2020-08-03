c211f964948aa384868690a0de02f8671907fc0a
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-03 08:38:23 -0300

    Password Strength

--- js/password-strength.js
+++ js/password-strength.js
@@ -1,24 +1,32 @@
 let score = 0;
 
-function hasRepetitions(){
+alert(hasRepetitions("aa"))
 
+function hasRepetitions(pass) {
+        var patt = /^([a-z])\1+$/;
+        var result = patt.test(pass);
+        return result;
 }
-function isLongEgnough(){
-    
+function isLongEgnough(password) {
+  if (password.length < 9) {
+    return false;
+  } else {
+    return true;
+  }
 }
-function hasCharacters(password){
-    var matchedCase = new Array();
-                matchedCase.push("[$@$!%*#?&]"); // Special Charector
-                matchedCase.push("[A-Z]");      // Uppercase Alpabates
-                matchedCase.push("[0-9]");      // Numbers
-                matchedCase.push("[a-z]");     // Lowercase Alphabates
+function hasCharacters(password) {
+  var matchedCase = new Array();
+  matchedCase.push("[$@$!%*#?&]"); // Special Charector
+  matchedCase.push("[A-Z]"); // Uppercase Alpabates
+  matchedCase.push("[0-9]"); // Numbers
+  matchedCase.push("[a-z]"); // Lowercase Alphabates
 
-                // Check the conditions
-                var ctr = 0;
-                for (var i = 0; i < matchedCase.length; i++) {
-                    if (new RegExp(matchedCase[i]).test(password)) {
-                        ctr = ctr + 10;
-                    }
-                }
-                score = score+ ctr;
+  // Check the conditions
+  var ctr = 0;
+  for (var i = 0; i < matchedCase.length; i++) {
+    if (new RegExp(matchedCase[i]).test(password)) {
+      ctr = ctr + 10;
+    }
+  }
+  score = score + ctr;
 }
