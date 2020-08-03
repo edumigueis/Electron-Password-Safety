f5a458f53b6faf23f351ade4c473e0d6adebe9a6
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-03 10:35:57 -0300

    Adjustments

--- js/password-generator.js
+++ js/password-generator.js
@@ -1,28 +1,35 @@
 let pass = "";
 document.getElementById("generate-pass").addEventListener("click", function () {
-   pass = genPass();
-  console.log(pass);
+  pass = genPass();
+  document.getElementById("pass-input").value = pass;
+  checkPassword(pass);
+  showPassStrength();
 });
 
 function genPass() {
-    var length = (12)?(12):(10);
-    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-    var numeric = '0123456789';
-    var punctuation = '!@#$%^&*()_+~`|}{[];?><,./-=';
-    var password = "";
-    var character = "";
-    var crunch = true;
-    while( password.length<length ) {
-        entity1 = Math.ceil(string.length * Math.random()*Math.random());
-        entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
-        entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
-        hold = string.charAt( entity1 );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-        character += hold;
-        character += numeric.charAt( entity2 );
-        character += punctuation.charAt( entity3 );
-        password = character;
-    }
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return password.substr(0,12);
+  var length = 12 ? 12 : 10;
+  var string = "abcdefghijklmnopqrstuvwxyz"; //to upper
+  var numeric = "0123456789";
+  var punctuation = "!@#$%^&*()_+~`|}{[];?><,./-=";
+  var password = "";
+  var character = "";
+  var crunch = true;
+  while (password.length < length) {
+    entity1 = Math.ceil(string.length * Math.random() * Math.random());
+    entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
+    entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
+    hold = string.charAt(entity1);
+    hold = password.length % 2 == 0 ? hold.toUpperCase() : hold;
+    character += hold;
+    character += numeric.charAt(entity2);
+    character += punctuation.charAt(entity3);
+    password = character;
+  }
+  password = password
+    .split("")
+    .sort(function () {
+      return 0.5 - Math.random();
+    })
+    .join("");
+  return password.substr(0, 12);
 }
--- js/password-strength.js
+++ js/password-strength.js
@@ -60,6 +60,7 @@ function hasCharacters(password) {
 }
 
 function checkPassword(pass) {
+  score = 0;
   if (isLongEgnough(pass)) {
     score = score + 20;
   }
@@ -83,46 +84,27 @@ function checkPassword(pass) {
 
 function showPassStrength() {
   var color;
-    if (0 < score < 10)
-      color = "#FF0000";
-else
-    if (10 < score < 20)
-      color = "#ED1C24";
-else
-    if (20 < score < 30)
-      color = "#C40233";
-else
-    if (30 < score < 40)
-      color = "#ED2939";
-else
-    if (40 < score < 50)
-      color = "#E44D2E";
-else
-    if (50 < score < 60)
-      color = "#FF8200";
-else
-    if (60 < score < 70)
-      color = "#FF9966";
-else
-    if (70 < score < 80)
-      color = "#71BC78";
-else
-    if (80 < score < 90)
-      color = "#228B22";
+  console.log(score);
+  if (score < 10) color = "#FF0000";
+  else if (score >= 10 && score < 20) color = "#ED1C24";
+  else if (score >= 20 && score < 30) color = "#C40233";
+  else if (score >= 30 && score < 40) color = "#ED2939";
+  else if (score >= 40 && score < 50) color = "#E44D2E";
+  else if (score >= 50 && score < 60) color = "#FF8200";
+  else if (score >= 60 && score < 70) color = "#FF9966";
+  else if (score >= 70 && score < 80) color = "#71BC78";
+  else if (score >= 80 && score < 90) color = "#85cd32";
+  else if (score >= 90 && score <= 100) color = "#228B22";
   document.getElementById("gradient").style.background = color;
   console.log(color);
 }
 
-document.getElementById("pass-input").addEventListener("change", function () {
-  let pass = document.getElementById("pass-input").value;
-  checkPassword(pass);
-  showPassStrength();
-});
 document.getElementById("pass-input").addEventListener("keyup", function () {
   let pass = document.getElementById("pass-input").value;
   checkPassword(pass);
   showPassStrength();
 });
+
 document.getElementById("pass-input").addEventListener("paste", function () {
   let pass = document.getElementById("pass-input").value;
   checkPassword(pass);
